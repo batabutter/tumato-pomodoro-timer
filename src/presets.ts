a3dbfc4c5337f80999c05af07afddcf50f1dae5a
bata_butter <113527212+batabutter@users.noreply.github.com>
2026-01-14 01:13:02 -0500

    added preset list functionality

--- src/presets.ts
+++ src/presets.ts
@@ -1,18 +1,24 @@
-const preset_list = document.getElementById("timer_preset_list") as HTMLDivElement
+const preset_list = document.getElementById("timer_preset_list") as HTMLDivElement;
+const li = document.getElementsByTagName("li");
+let listElements:HTMLElement[] = [];
+let selected = -1;
+let currentTarget:HTMLElement | null = null;
 
 const renderPresetList = async () => {
-    
+
     try {
         const res = await fetch("../customization/timer-presets.json");
         const json = await res.json();
 
-        preset_list.innerHTML += 
+        preset_list.innerHTML +=
             `<ul> ${json.presets.map(
-                        (timer:TimerData) => 
-                            (`<li><p>${timer.name}</p><p>Work: ${timer.workDuration}</p><p>Break: ${timer.breakDuration}</p></li>`)).join('')}
+                (timer: TimerData) =>
+                    (`<li><p>${timer.name}</p><p>Work: ${timer.workDuration}</p><p>Break: ${timer.breakDuration}</p></li>`)).join('')}
             </ul>`;
-            
+
         console.log("Made it here");
+
+        listElements = Array.from(preset_list.querySelectorAll("li"));
 
 
     } catch (error) {
@@ -23,5 +29,25 @@ const renderPresetList = async () => {
 const savePreset = () => {
 
 }
+
+preset_list.addEventListener("click",
+    function (e: PointerEvent) {
+        const target = (e.target as HTMLElement).closest('li');
+
+        if (target) {
+            const index = listElements.indexOf(target);
+
+            if (selected !== index) {
+
+                if (currentTarget)
+                    currentTarget.classList.remove("list_selection_border")
+
+                currentTarget = target;
+                selected = index;
+                console.log(`Selected: ${index}`);
+                currentTarget.classList.add("list_selection_border")
+            }
+        }
+    })
 
 renderPresetList();
