d75507f15fc6430ea2ad4f37ed11bfc54b284f3c
bata_butter <113527212+batabutter@users.noreply.github.com>
2026-01-14 00:09:02 -0500

    Added switch button and altered play button

--- src/timer.ts
+++ src/timer.ts
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("timer") as HTMLCanvasElement;
 const start_button = document.getElementById("start_button") as HTMLButtonElement;
-const stop_button = document.getElementById("stop_button") as HTMLButtonElement;
+const start_icon = document.getElementById("start_icon") as HTMLButtonElement;
+const switch_button = document.getElementById("switch_button") as HTMLButtonElement;
 const reset_button = document.getElementById("reset_button") as HTMLButtonElement;
 
 const ctx = canvas ? canvas.getContext("2d") : null;
@@ -13,20 +14,69 @@ const breakTimerSize = (1 - ratio) * canvas.width
 const lineWidth = 15;
 const workTimerOuterRadius = Math.min((workTimerSize) / 2, (workTimerSize) / 2);
 const breakTimerOuterRadius = (breakTimerSize) / 2;
+
 const color_scheme_bg = "rgb(80, 80, 80)";
-const color_scheme_font = "rgb(0, 255, 213)";
+const color_scheme_work = "rgb(0, 255, 213)";
+const color_scheme_break = "rgb(255, 99, 99)";
 const color_scheme_canvas = parentElement ? getComputedStyle(parentElement).backgroundColor : "white";
 
 let requestId: number = -1;
+
+enum TimerType {
+    Work,
+    Break
+}
+
+class Timer {
+
+    type: TimerType;
+    startTime: number | null;
+    pausedStart: number | null;
+    pausedTime: number;
+    elaspedTime: number;
+    totalTime: number;
+    isRunning: boolean;
+
+    constructor(timerType: TimerType, totalTime: number) {
+        this.type = timerType;
+        this.startTime = null;
+        this.pausedStart = null;
+        this.pausedTime = 0;
+        this.elaspedTime = 0;
+        this.totalTime = totalTime;
+        this.isRunning = false;
+    }
+
+    GetTimerType() { return this.type; }
+    GetStartTime() { return this.startTime; }
+    GetPausedStart() { return this.pausedStart; }
+    GetPausedTime() { return this.pausedTime; }
+    GetTotalTime() { return this.totalTime; }
+    GetElaspedTime() { return this.elaspedTime; }
+    GetIsRunning() { return this.isRunning; }
+
+    SetStartTime(time: number) { this.startTime = time; }
+    SetPausedStart(time: number) { this.pausedStart = time; }
+    SetPausedTime(time: number) { this.pausedTime = time; }
+    SetElaspsedTime(time: number) { this.elaspedTime = time; }
+    SetIsRunning(running: boolean) { this.isRunning = running; }
+
+    ResetAll() {
+        this.startTime = null;
+        this.pausedStart = null;
+        this.pausedTime = 0;
+        this.elaspedTime = 0;
+    }
+
+}
+
 let totalWorkTime = 10000;
 let totalBreakTime = 10000;
+const WorkTimer = new Timer(TimerType.Work, totalWorkTime);
+const BreakTimer = new Timer(TimerType.Break, totalBreakTime);
 
-let pausedTime: number = 0;
-let pausedStart: number | null = null;
-let startTime: number | null = null;
-
-let elaspedWorkTime = 0;
-let elaspedBreakTime = 0;
+let FocusedTimer: Timer | null;
+FocusedTimer = WorkTimer;
 
 const formatTime = (remainingTime: number): string => {
 
@@ -43,7 +93,7 @@ const formatTime = (remainingTime: number): string => {
 const draw_background = () => {
     const time = new Date();
 
-    if (!ctx) return;
+    if (!ctx || !FocusedTimer) return;
 
     ctx.fillStyle = color_scheme_canvas;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -56,7 +106,9 @@ const draw_background = () => {
     ctx.arc(
         breakTimerOuterRadius + (canvas.width - breakTimerSize),
         breakTimerOuterRadius,
-        breakTimerOuterRadius - lineWidth / 2, 0, 2 * Math.PI);
+        breakTimerOuterRadius - lineWidth / 2,
+        0,
+        2 * Math.PI);
     ctx.stroke();
 
     // Draw the work timer
@@ -68,57 +120,92 @@ const draw_background = () => {
     ctx.arc(
         workTimerOuterRadius,
         workTimerOuterRadius + (canvas.width - workTimerSize),
-        workTimerOuterRadius - lineWidth / 2, 0, 2 * Math.PI);
+        workTimerOuterRadius - lineWidth / 2,
+        0,
+        2 * Math.PI);
     ctx.stroke();
 
+    // Work Timer 
+    drawTimer(
+        (WorkTimer.GetElaspedTime() / WorkTimer.GetTotalTime()),
+        workTimerOuterRadius,
+        workTimerOuterRadius + (canvas.width - workTimerSize),
+        workTimerOuterRadius - lineWidth / 2,
+        color_scheme_work);
+
+    // Break Timer
+    drawTimer(
+        (BreakTimer.GetElaspedTime() / BreakTimer.GetTotalTime()),
+        breakTimerOuterRadius + (canvas.width - breakTimerSize),
+        breakTimerOuterRadius,
+        breakTimerOuterRadius - lineWidth / 2,
+        color_scheme_break);
+
+    /* Focused Timer must be initalized beforehand */
     draw_time();
+    
+    /* This should be optimized */
+    if (FocusedTimer.GetIsRunning()) {
+        start_icon.classList.remove("fa-play");
+        start_icon.classList.add("fa-pause");
+    } else {
+        start_icon.classList.remove("fa-pause");
+        start_icon.classList.add("fa-play");
+    }
 
     ctx.save();
 }
 
-const drawWorkTimer = (fraction: number) => {
+const drawTimer = (fraction: number, x: number, y: number, radius: number, color: string) => {
     if (!ctx) return
 
     ctx.beginPath();
-
-    ctx.strokeStyle = color_scheme_font;
+    ctx.strokeStyle = color;
     ctx.arc(
-        workTimerOuterRadius,
-        workTimerOuterRadius + (canvas.width - workTimerSize),
-        workTimerOuterRadius - lineWidth / 2, -(Math.PI / 2), ((2 * Math.PI) * fraction) - (Math.PI / 2));
+        x,
+        y,
+        radius,
+        -(Math.PI / 2),
+        ((2 * Math.PI) * fraction) - (Math.PI / 2));
     ctx.stroke();
 }
 
-const drawBreakTimer = () => {
-
-}
-
-
 const start_timer = () => {
-    if (!ctx) return;
+    if (!ctx || !FocusedTimer) return;
 
-    if (!startTime) {
-        startTime = Date.now();
+    if (FocusedTimer.GetIsRunning()) {
+        stop_timer();
         draw_background();
-    } else if (pausedStart) {
-        pausedTime = Date.now() - pausedStart;
-        pausedStart = 0;
+        return;
     }
 
+    const pausedStart = FocusedTimer.GetPausedStart();
+    FocusedTimer.SetIsRunning(true);
+
+    if (!FocusedTimer.GetStartTime()) {
+        FocusedTimer.SetStartTime(Date.now());
+        draw_background();
+    } else if (pausedStart) {
+        FocusedTimer.SetPausedTime(
+            FocusedTimer.GetPausedTime() +
+            Date.now() - pausedStart
+        );
+        FocusedTimer.SetPausedStart(0);
+    }
+
+    const startTime = FocusedTimer.GetStartTime();
+    const pausedTime = FocusedTimer.GetPausedTime();
+
     const run_timer = () => {
+        if (!startTime || !FocusedTimer) return;
+
         const now_time = Date.now();
-        if (!startTime || (elaspedWorkTime) >= totalWorkTime) return;
 
         const delta_time = (now_time - startTime - pausedTime);
-        const fraction = (delta_time) / totalWorkTime;
+        const fraction = (delta_time) / FocusedTimer.GetTotalTime();
 
-        elaspedWorkTime = now_time - startTime;
+        FocusedTimer.SetElaspsedTime(now_time - startTime - pausedTime);
         draw_background();
-
-        drawWorkTimer(fraction);
-        drawBreakTimer();
-
-        elaspedWorkTime = now_time - startTime;
 
         if (fraction >= 1) {
             window.cancelAnimationFrame(requestId)
@@ -132,19 +219,17 @@ const start_timer = () => {
 }
 
 const stop_timer = () => {
-    if (!ctx || !startTime) return;
+    if (!ctx || !FocusedTimer) return;
 
-    pausedStart = Date.now();
-    window.cancelAnimationFrame(requestId)
+    FocusedTimer.SetIsRunning(false)
+    FocusedTimer.SetPausedStart(Date.now());
+    window.cancelAnimationFrame(requestId);
 }
 
 const reset_timer = () => {
-    if (!ctx) return;
+    if (!ctx || !FocusedTimer) return;
 
-    startTime = null;
-    pausedTime = 0;
-    pausedStart = null;
-    elaspedWorkTime = 0;
+    FocusedTimer.ResetAll();
 
     ctx.fillStyle = color_scheme_canvas;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -155,8 +240,9 @@ const reset_timer = () => {
 }
 
 /* The fonts here are hard-coded, kinda lame */
+/* Focused Timer must be initalized beforehand */
 const draw_time = () => {
-    if (!ctx) return;
+    if (!ctx || !FocusedTimer) return;
 
     ctx.font = "bold 5em serif";
     ctx.textBaseline = "middle";
@@ -164,25 +250,45 @@ const draw_time = () => {
 
     ctx.fillStyle = color_scheme_bg;
 
-    const remainingWorkTime = totalWorkTime - elaspedWorkTime;
+    const remainingWorkTime = WorkTimer.GetTotalTime() - WorkTimer.GetElaspedTime();
+    const remainingBreakTime = BreakTimer.GetTotalTime() - BreakTimer.GetElaspedTime();
 
-    const formattedTime = formatTime(remainingWorkTime);
+    const formattedWorkTime = formatTime(remainingWorkTime);
+    const formattedBreakTime = formatTime(remainingBreakTime);
+
+    if (FocusedTimer.GetTimerType() == TimerType.Work)
+        ctx.fillStyle = "white";
 
     ctx.fillText(
-        formattedTime,
+        formattedWorkTime,
         workTimerSize / 2,
         workTimerOuterRadius + (canvas.width - workTimerSize));
 
+    ctx.fillStyle = color_scheme_bg;
+
+    if (FocusedTimer.GetTimerType() == TimerType.Break)
+        ctx.fillStyle = "white";
+
     ctx.font = "bold 1.25em serif";
     ctx.fillText(
-        formattedTime,
+        formattedBreakTime,
         breakTimerOuterRadius + (canvas.width - breakTimerSize),
         breakTimerOuterRadius);
 }
 
+const switch_timer = () => {
+    stop_timer();
+
+    if (FocusedTimer == WorkTimer)
+        FocusedTimer = BreakTimer;
+    else
+        FocusedTimer = WorkTimer;
+
+    draw_background();
+}
 
 start_button.addEventListener("click", start_timer);
-stop_button.addEventListener("click", stop_timer);
+switch_button.addEventListener("click", switch_timer);
 reset_button.addEventListener("click", reset_timer);
 
 draw_background();
