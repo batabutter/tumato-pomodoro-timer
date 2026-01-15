7a60243aece2d84eb2c7aec3e4f903bf64aa1c35
bata_butter <113527212+batabutter@users.noreply.github.com>
2026-01-15 06:32:46 -0500

    packaged project

---
+++ forge.config.js
@@ -0,0 +1,55 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const path = require("path")
+
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    icon: path.join(__dirname, "/assets/tumato")
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        icon: path.join(__dirname, "/assets/tumato.ico"),
+        author: "michaelmorton",
+        setupIcon: path.join(__dirname, "/assets/tumato.ico"),
+        description: "Tumato - A virtual pomodoro timer"
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {
+        options: {
+          icon: path.join(__dirname, "tumato.png")
+        }
+      },
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+};
